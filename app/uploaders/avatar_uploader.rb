168950fefb1124370ac1fd79d8e1570bd7a15cf1
Ryan <hiphubryan@gmail.com>
2015-05-10 20:50:08 +0800

    Move carrierwave config to uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,6 +6,23 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
+    # #Add for Heroku, It should be deleted after migrate to AWS
+  CarrierWave.configure do |config|
+   
+    config.storage :fog
+
+    config.fog_credentials = {
+      :provider               => 'AWS',                        # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
+    }
+    config.fog_directory  = ENV['AWS_BUCKET']                   # required
+
+
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  end
+  ###################################################################
+
   # Choose what kind of storage to use for this uploader:
   #storage :file
   # storage :fog
